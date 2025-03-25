765f97057b07ef4552d502dd0e05516e5a9b316d
Matheus Vieira <matheus.msv1997@gmail.com>
2025-03-25 00:22:45 -0300

    feat: migrate API configuration from .env file to environment files for production and development

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    CARD_LIST_URL: 'https://githubanotaai.github.io/frontend-interview-mock-data/cardlist.json'
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    CARD_LIST_URL: 'https://githubanotaai.github.io/frontend-interview-mock-data/cardlist.json'
+};
