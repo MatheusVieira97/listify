83cd6a0c8c40098013ef3e4794ee9974252a45a2
Matheus Vieira <matheus.msv1997@gmail.com>
2025-03-26 09:40:47 -0300

    style: update card interface formatting and add medium-small font size variable in SCSS

--- src/app/shared/models/card.ts
+++ src/app/shared/models/card.ts
@@ -1,12 +1,12 @@
 export interface Card {
-    title: string;
-    description: string;
-    img: string;
-    type: CardType;
+  title: string;
+  description: string;
+  img: string;
+  type: CardType;
 }
 
 export enum CardType {
-    Paisagem = 1,
-    Flor = 2,  
-    Pizza = 3   
+  Paisagem = 1,
+  Flor = 2,
+  Pizza = 3,
 }
