ec4e91736902e304c87a4dacb20253bdb6bac324
Matheus Vieira <matheus.msv1997@gmail.com>
2025-03-26 09:41:37 -0300

    feat: enhance HomeComponent with card deletion functionality and responsive styling adjustments

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,20 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HeaderComponent } from '../../components/header/header.component';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { ContentService } from '../../services/content.service';
+import { of } from 'rxjs';
+import { Card, CardType } from '../../shared/models/card';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let contentService: jasmine.SpyObj<ContentService>;
+
+  const mockCards: Card[] = [
+    {
+      title: 'Test Card 1',
+      description: 'Description 1',
+      img: 'test1.jpg',
+      type: CardType.Paisagem
+    },
+    {
+      title: 'Test Card 2',
+      description: 'Description 2',
+      img: 'test2.jpg',
+      type: CardType.Flor
+    }
+  ];
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        HomeComponent, 
-        HeaderComponent
-      ]
-    })
-    .compileComponents();
+    const contentServiceSpy = jasmine.createSpyObj('ContentService', ['getCardList']);
+    contentServiceSpy.getCardList.and.returnValue(of(mockCards));
 
+    await TestBed.configureTestingModule({
+      imports: [HomeComponent],
+      providers: [
+        { provide: ContentService, useValue: contentServiceSpy },
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    }).compileComponents();
+
+    contentService = TestBed.inject(ContentService) as jasmine.SpyObj<ContentService>;
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,5 +47,25 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should filter cards based on search term', fakeAsync(() => {
+    component.allCards = mockCards;
+    component.searchControl.setValue('Test Card 1');
+    component.onSearchChange();
+    tick();
+    expect(component.filteredCards.length).toBe(1);
+    expect(component.filteredCards[0]).toEqual(mockCards[0]);
+  }));
+
+  it('should delete card', () => {
+    component.allCards = [...mockCards];
+    component.filteredCards = [...mockCards];
+    const cardToDelete = mockCards[0];
+    
+    component.onDeleteCard(cardToDelete);
+    
+    expect(component.allCards).not.toContain(cardToDelete);
+    expect(component.filteredCards).not.toContain(cardToDelete);
   });
 });
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
-import { CardListComponent } from "../../components/card-list/card-list.component";
-import { Card } from "../../shared/models/card";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { CardListComponent } from '../../components/card-list/card-list.component';
+import { Card } from '../../shared/models/card';
 import { ContentService } from '../../services/content.service';
 import { FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -12,24 +12,35 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [HeaderComponent, CardListComponent, ReactiveFormsModule],
   providers: [ContentService],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   filteredCards: Card[] = [];
   allCards: Card[] = [];
   searchControl = new FormControl('');
 
-  constructor(private contentService: ContentService) {
-    this.contentService.getCardList().subscribe(cards => {
-      this.filteredCards = cards;
+  constructor(private contentService: ContentService) {}
+
+  ngOnInit(): void {
+    this.contentService.getCardList().subscribe((cards: Card[]) => {
       this.allCards = cards;
+      this.filteredCards = cards;
     });
   }
 
   onSearchChange() {
     const searchTerm = this.searchControl.value?.toLowerCase() || '';
-    this.filteredCards = this.allCards.filter(card => 
-      card.title.toLowerCase().includes(searchTerm) || card.description.toLowerCase().includes(searchTerm)
+    this.filteredCards = this.allCards.filter(
+      (card) =>
+        card.title.toLowerCase().includes(searchTerm) ||
+        card.description.toLowerCase().includes(searchTerm),
+    );
+  }
+
+  onDeleteCard(cardToDelete: Card) {
+    this.allCards = this.allCards.filter((card) => card !== cardToDelete);
+    this.filteredCards = this.filteredCards.filter(
+      (card) => card !== cardToDelete,
     );
   }
 }
