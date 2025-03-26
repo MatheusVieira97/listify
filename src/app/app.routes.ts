94c529c8c140e4a5400294df2f001c8a89160ad8
Matheus Vieira <matheus.msv1997@gmail.com>
2025-03-26 09:40:34 -0300

    style: fix formatting issues in app component, config, and routes files for consistency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: '**', component: NotFoundComponent }
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent },
 ];
